f6d732c2d9f4eb9cb07578c092784f74788efb3f
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-09-10 16:40:48 +0000

    initial setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wavly/shawty
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const PORT string = "1234"
+
+func main() {
+  router := http.NewServeMux()
+
+  router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("pong\n"))
+  })
+  
+	fmt.Println("Listening on:", PORT)
+	if err := http.ListenAndServe("0.0.0.0:"+PORT, router); err != nil {
+		log.Fatalln("Failed to start the server:", err)
+	}
+}
