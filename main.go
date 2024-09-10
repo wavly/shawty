ad4350c025a5fe41ea5f0d9ddb8c3639e5743637
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-09-10 16:47:34 +0000

    fix: formatting issues

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+  "fmt"
+  "log"
+  "net/http"
 )
 
 const PORT string = "1234"
@@ -14,9 +14,9 @@ func main() {
   router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("pong\n"))
   })
-  
-	fmt.Println("Listening on:", PORT)
-	if err := http.ListenAndServe("0.0.0.0:"+PORT, router); err != nil {
-		log.Fatalln("Failed to start the server:", err)
-	}
+
+  fmt.Println("Listening on:", PORT)
+  if err := http.ListenAndServe("0.0.0.0:"+PORT, router); err != nil {
+    log.Fatalln("Failed to start the server:", err)
+  }
 }
