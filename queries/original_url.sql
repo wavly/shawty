d608ea06e3d128266677e4bdc4311436d9562d6c
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-09-16 10:48:42 +0000

    feat: query for getting the original URL based on the code

--- queries/original_url.sql
+++ queries/original_url.sql
@@ -1,2 +1,5 @@
+-- name: GetOriginalUrl :one
+SELECT original_url FROM urls where code = ?;
+
 -- name: UpdateAccessedAndLastCount :exec
 UPDATE urls SET accessed_count = accessed_count + 1, last_accessed = ? WHERE code = ?
