25578a28777bb5a49691f14862cb1b6beab86b4f
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-09-16 11:03:15 +0000

    feat: query for creating new short link

--- queries/original_url.sql
+++ queries/original_url.sql
@@ -2,4 +2,11 @@
 SELECT original_url FROM urls where code = ?;
 
 -- name: UpdateAccessedAndLastCount :exec
-UPDATE urls SET accessed_count = accessed_count + 1, last_accessed = ? WHERE code = ?
+UPDATE urls SET accessed_count = accessed_count + 1, last_accessed = ? WHERE code = ?;
+
+-- name: CreateShortLink :one
+INSERT INTO urls (
+  original_url,
+  code
+) VALUES ( ?, ? )
+RETURNING *;
