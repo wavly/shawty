573362ab561cce56669aa3c10491824d75688bab
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-09-18 06:41:16 +0000

    refactor: using validate module for validating the URL

--- handlers/shawty.go
+++ handlers/shawty.go
@@ -7,12 +7,12 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"net/url"
 	"strings"
 
 	"github.com/wavly/shawty/asserts"
 	"github.com/wavly/shawty/internal/database"
 	"github.com/wavly/shawty/utils"
+	"github.com/wavly/shawty/validate"
 )
 
 type ShortLink struct {
@@ -21,39 +21,23 @@ type ShortLink struct {
 }
 
 func Shawty(w http.ResponseWriter, r *http.Request) {
-	longUrl := r.FormValue("url")
+	inputUrl := r.FormValue("url")
 
 	// Check if longUrl contains "://" and add "https://" if missing
-	if !strings.Contains(longUrl, "://") {
-		longUrl = "https://" + longUrl
+	if !strings.Contains(inputUrl, "://") {
+		inputUrl = "https://" + inputUrl
 	}
 
 	errorTempl := template.Must(template.ParseFiles("./partial-html/short-link-error.html"))
 
-	// Parse the URL to validate it and check its scheme
-	parsedUrl, err := url.Parse(longUrl)
-	if err != nil || parsedUrl.Scheme != "https" {
-		asserts.NoErr(errorTempl.Execute(w, "Invalid URL. Only HTTPS schema is allowed"), "Failed to execute template short-link-error.html")
-		return
-	}
-
-	// Check if URL contains a TLD (Top-Level Domain)
-	if !strings.Contains(longUrl, ".") {
-		asserts.NoErr(errorTempl.Execute(w, "The URL doesn't contain TLD (Top-Level Domain)"), "Failed to execute template short-link-error.html")
-		return
-	} else if split := strings.SplitN(longUrl, ".", 2); split[1] == "" {
-		asserts.NoErr(errorTempl.Execute(w, "The URL doesn't contain TLD (Top-Level Domain)"), "Failed to execute template short-link-error.html")
-		return
-	}
-
-	// Check the lenght of the URL
-	if len(longUrl) > 1000 {
-		asserts.NoErr(errorTempl.Execute(w, "The URL is too long, Max URL lenght is 1000 characters"), "Failed to execute template short-link-error.html")
+	err := validate.ValidateUrl(inputUrl)
+	if err != nil {
+		asserts.NoErr(errorTempl.Execute(w, err.Error()), "Failed to execute template short-link-error.html")
 		return
 	}
 
 	hasher := sha256.New()
-	hasher.Write([]byte(longUrl))
+	hasher.Write([]byte(inputUrl))
 	checksum := hasher.Sum(nil)
 
 	db := utils.ConnectDB()
@@ -66,9 +50,8 @@ func Shawty(w http.ResponseWriter, r *http.Request) {
 	templ := template.Must(template.ParseFiles("./partial-html/short-link.html"))
 
 	// Check if the url exists in the database
-	row := db.QueryRow("select code from urls where code = ?", hashUrl)
-	var code string
-	if err := row.Scan(&code); err != nil {
+	code, err := queries.GetCode(r.Context(), hashUrl)
+	if err != nil {
 		// Check if err doesn't equal to `sql.ErrNoRows`
 		// And if true then log the error and return
 		if err != sql.ErrNoRows {
@@ -79,7 +62,7 @@ func Shawty(w http.ResponseWriter, r *http.Request) {
 
 		// Insert the URL in the database if doesn't exists
 		_, err = queries.CreateShortLink(r.Context(), database.CreateShortLinkParams{
-			OriginalUrl: longUrl,
+			OriginalUrl: inputUrl,
 			Code:        hashUrl,
 		})
 		if err != nil {
@@ -89,20 +72,18 @@ func Shawty(w http.ResponseWriter, r *http.Request) {
 		}
 
 		w.WriteHeader(http.StatusCreated)
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		data := ShortLink{
 			ShortUrl:    hashUrl,
-			OriginalUrl: longUrl,
+			OriginalUrl: inputUrl,
 		}
 		asserts.NoErr(templ.Execute(w, data), "Failed to execute template short-link.html")
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	data := ShortLink{
 		ShortUrl:    code,
-		OriginalUrl: longUrl,
+		OriginalUrl: inputUrl,
 	}
 	asserts.NoErr(templ.Execute(w, data), "Failed to execute template short-link.html")
 }
