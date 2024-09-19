c7ecdc57aec0209c01e2bf5e1b44cc61cce30f4e
statulr <122219240+statulr@users.noreply.github.com>
2024-09-19 13:54:59 -0400

    update: more function documentation for assert

--- asserts/assert.go
+++ asserts/assert.go
@@ -1,13 +1,20 @@
 package asserts
 
-import "log"
+import (
+    "log"
+)
 
 // NoErr logs a fatal error message along with any provided error details.
 //
-// Checks if an error is non-nil and logs both a custom message and the error details,
-// then terminates the program.
+// This function checks if an error is non-nil. If the error is non-nil, it logs both
+// a custom message and the error details, then terminates the program.
+//
+// Parameters:
+// - err: The error to check.
+// - msg: A custom error message.
+// - log.Fatalf: Logs the fatal error message details and in this case your custom message and the error
 func NoErr(err error, msg string) {
-	if err != nil {
-		log.Fatalln(msg, err)
-	}
+    if err != nil {
+        log.Fatalf("%s: %v", msg, err)
+    }
 }
