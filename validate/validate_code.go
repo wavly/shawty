e3b50eefe9498a7d7693eb43197d73302304c3a6
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-09-23 15:57:34 +0000

    feat: checking the min length of the code

--- validate/validate_code.go
+++ validate/validate_code.go
@@ -10,12 +10,20 @@ type TooLong struct {
 	lenght uint
 }
 
+type TooShort struct {
+	lenght uint
+}
+
 type NotAplphaOrNum struct {
 	text string
 }
 
 func (code *TooLong) Error() string {
 	return fmt.Sprintf("Max lenght of the code is 8, but got %v", code.lenght)
+}
+
+func (_ *TooShort) Error() string {
+	return "Min lenght of the code is 2"
 }
 
 func (code *NotAplphaOrNum) Error() string {
@@ -25,6 +33,8 @@ func (code *NotAplphaOrNum) Error() string {
 func CustomCodeValidate(code string) error {
 	if len(code) > 8 {
 		return &TooLong{lenght: uint(len(code))}
+	} else if len(code) < 2 {
+		return &TooShort{}
 	}
 
 	if !utils.IsAplphabetOrNum(code) {
