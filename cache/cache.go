41a91bf6dc0139428ba6f4e9c9ad73ee9e89a0e7
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-10-02 06:11:10 +0000

    feat: storing the original URL redirect request in cache

---
+++ cache/cache.go
@@ -0,0 +1,9 @@
+package cache
+
+import (
+	"time"
+
+	"github.com/patrickmn/go-cache"
+)
+
+var Cache = cache.New(5*time.Minute, 10*time.Minute)
--- handlers/redirect.go
+++ handlers/redirect.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/patrickmn/go-cache"
+	. "github.com/wavly/shawty/cache"
 	"github.com/wavly/shawty/internal/database"
 	"github.com/wavly/shawty/utils"
 	"github.com/wavly/shawty/validate"
@@ -25,15 +27,26 @@ func Redirect(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 	queries := database.New(db)
 
-	originalUrl, err := queries.GetOriginalUrl(r.Context(), code)
-	if err != nil {
-		utils.ServerErrTempl(w, "Sorry, an unexpected error occur when querying the database for the URL")
-		log.Println("Error: when querying the database for the URL", err)
+	// Try to get the original URL from cache
+	originalUrl, found := Cache.Get(code)
+
+	// Update the cache if the doesn't exist
+	if !found {
+		originalUrl, err := queries.GetOriginalUrl(r.Context(), code)
+		if err != nil {
+			utils.ServerErrTempl(w, "Sorry, an unexpected error occur when querying the database for the URL")
+			log.Println("Error: when querying the database for the URL", err)
+			return
+		}
+
+		http.Redirect(w, r, originalUrl, http.StatusSeeOther)
+		Cache.Set(code, originalUrl, cache.DefaultExpiration)
 		return
 	}
 
-	http.Redirect(w, r, originalUrl, http.StatusSeeOther)
+	http.Redirect(w, r, originalUrl.(string), http.StatusSeeOther)
 
+	// Todo: update last-time/access count in cache
 	// Update the accessed_count and last_accessed in one query
 	err = queries.UpdateAccessedAndLastCount(r.Context(), database.UpdateAccessedAndLastCountParams{
 		Code:         code,
