92c25352d98379ad62051361b8fd413a414666df
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-10-17 08:24:16 +0000

    feat: impl custom pretty logger

---
+++ pretty-logger/log.go
@@ -0,0 +1,133 @@
+package prettylogger
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"strconv"
+	"sync"
+)
+
+const (
+	reset      = "\033[0m"
+	timeFormat = "[15:04:05.000]"
+
+	black        = 30
+	red          = 31
+	green        = 32
+	yellow       = 33
+	blue         = 34
+	magenta      = 35
+	cyan         = 36
+	lightGray    = 37
+	darkGray     = 90
+	lightRed     = 91
+	lightGreen   = 92
+	lightYellow  = 93
+	lightBlue    = 94
+	lightMagenta = 95
+	lightCyan    = 96
+	white        = 97
+)
+
+type Handler struct {
+	handler slog.Handler
+	buf     *bytes.Buffer
+	mutex   *sync.Mutex
+}
+
+func colorize(colorCode int, v string) string {
+	return fmt.Sprintf("\033[%sm%s%s", strconv.Itoa(colorCode), v, reset)
+}
+
+func (h *Handler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.handler.Enabled(ctx, level)
+}
+
+func (h *Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &Handler{handler: h.handler.WithAttrs(attrs), buf: h.buf, mutex: h.mutex}
+}
+
+func (h *Handler) WithGroup(name string) slog.Handler {
+	return &Handler{handler: h.handler.WithGroup(name), buf: h.buf, mutex: h.mutex}
+}
+
+func (h *Handler) Handle(ctx context.Context, r slog.Record) error {
+
+	level := r.Level.String() + ":"
+
+	switch r.Level {
+	case slog.LevelDebug:
+		level = colorize(darkGray, level)
+	case slog.LevelInfo:
+		level = colorize(cyan, level)
+	case slog.LevelWarn:
+		level = colorize(lightYellow, level)
+	case slog.LevelError:
+		level = colorize(lightRed, level)
+	}
+
+	fmt.Println(
+		colorize(lightGray, r.Time.Format(timeFormat)),
+		level,
+		colorize(white, r.Message),
+	)
+
+	return nil
+}
+
+func (h *Handler) computeAttrs(
+	ctx context.Context,
+	r slog.Record,
+) (map[string]any, error) {
+
+	h.mutex.Lock()
+	defer func() {
+		h.buf.Reset()
+		h.mutex.Unlock()
+	}()
+	if err := h.handler.Handle(ctx, r); err != nil {
+		return nil, fmt.Errorf("error when calling inner handler's Handle: %w", err)
+	}
+
+	var attrs map[string]any
+	err := json.Unmarshal(h.buf.Bytes(), &attrs)
+	if err != nil {
+		return nil, fmt.Errorf("error when unmarshaling inner handler's Handle result: %w", err)
+	}
+	return attrs, nil
+}
+
+func suppressDefaults(
+	next func([]string, slog.Attr) slog.Attr,
+) func([]string, slog.Attr) slog.Attr {
+	return func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.TimeKey ||
+			a.Key == slog.LevelKey ||
+			a.Key == slog.MessageKey {
+			return slog.Attr{}
+		}
+		if next == nil {
+			return a
+		}
+		return next(groups, a)
+	}
+}
+
+func NewHandler(opts *slog.HandlerOptions) *Handler {
+	if opts == nil {
+		opts = &slog.HandlerOptions{}
+	}
+	b := &bytes.Buffer{}
+	return &Handler{
+		buf: b,
+		handler: slog.NewJSONHandler(b, &slog.HandlerOptions{
+			Level:       opts.Level,
+			AddSource:   opts.AddSource,
+			ReplaceAttr: suppressDefaults(opts.ReplaceAttr),
+		}),
+		mutex: &sync.Mutex{},
+	}
+}
