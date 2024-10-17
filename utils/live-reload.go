fed68f4d2561f188a7b99b6f7247d004703c3718
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-10-17 10:43:40 +0000

    feat: dynamically generating the dev script for live-reloading

--- utils/live-reload.go
+++ utils/live-reload.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"bytes"
+	"fmt"
 	"html/template"
 	"os"
 	"path/filepath"
@@ -8,8 +10,6 @@ import (
 	"github.com/wavly/shawty/asserts"
 	"github.com/wavly/shawty/env"
 )
-
-const script = `<script src="/static/live-reload.js"></script>`
 
 // Injects a script into the [Template File] to live-reload the page if in `DEV` mode,
 // else just returns the file as it is.
@@ -20,7 +20,11 @@ func Templ(path string) *template.Template {
 	content, err := os.ReadFile(path)
 	asserts.NoErr(err, "Failed to read file")
 
-	content = append(content, []byte(script)...)
+	script := template.Must(template.ParseFiles("./static/live-reload.js"))
+	var buffer bytes.Buffer
+	script.Execute(&buffer, env.PORT)
+
+	content = append(content, []byte(formatScript(buffer))...)
 	ret, err := template.New(filepath.Base(path)).Parse(string(content))
 	asserts.NoErr(err, "Faield to parse template")
 	return ret
@@ -38,6 +42,14 @@ func StaticFile(path string) []byte {
 	content, err := os.ReadFile(path)
 	asserts.NoErr(err, "Failed to read file")
 
-	content = append(content, []byte(script)...)
+	script := template.Must(template.ParseFiles("./static/live-reload.js"))
+	var buffer bytes.Buffer
+	script.Execute(&buffer, env.PORT)
+
+	content = append(content, []byte(formatScript(buffer))...)
 	return content
+}
+
+func formatScript(buf bytes.Buffer) string {
+	return fmt.Sprintf("<script>%s</script>", buf.String())	
 }
