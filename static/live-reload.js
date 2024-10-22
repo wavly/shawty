8ad6272cac7bd59dabca92e34c47aac245299f85
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-10-22 15:14:04 +0000

    feat: refactor the live-reload script

--- static/live-reload.js
+++ static/live-reload.js
@@ -1,29 +1,28 @@
-(() => {
-  const socketUrl = 'ws://localhost:{{ . }}/dev';
+const socketUrl = 'ws://localhost:{{ . }}/dev';
 
-  let socket = new WebSocket(socketUrl);
+let socket = new WebSocket(socketUrl);
 
-  socket.addEventListener('close', () => {
-    const interAttemptTimeoutMilliseconds = 100;
-    const maxDisconnectedTimeMilliseconds = 3000;
-    const maxAttempts = Math.round(
-      maxDisconnectedTimeMilliseconds / interAttemptTimeoutMilliseconds,
-    );
-    let attempts = 0;
-    const reloadIfCanConnect = () => {
-      attempts++;
-      if (attempts > maxAttempts) {
-	console.error('Could not reconnect to dev server.');
-	return;
-      }
-      socket = new WebSocket(socketUrl);
-      socket.addEventListener('error', () => {
-	setTimeout(reloadIfCanConnect, interAttemptTimeoutMilliseconds);
-      });
-      socket.addEventListener('open', () => {
-	location.reload();
-      });
-    };
-    reloadIfCanConnect();
-  });
-})();
+socket.addEventListener('close', () => {
+  const interAttemptTimeoutMilliseconds = 100;
+  const maxDisconnectedTimeMilliseconds = 3000;
+  const maxAttempts = Math.round(
+    maxDisconnectedTimeMilliseconds / interAttemptTimeoutMilliseconds,
+  );
+
+  let attempts = 0;
+  const reloadIfCanConnect = () => {
+    attempts++;
+    if (attempts > maxAttempts) {
+      console.error('Could not reconnect to dev server.');
+      return;
+    }
+    socket = new WebSocket(socketUrl);
+    socket.addEventListener('error', () => {
+      setTimeout(reloadIfCanConnect, interAttemptTimeoutMilliseconds);
+    });
+    socket.addEventListener('open', () => {
+      location.reload();
+    });
+  };
+  reloadIfCanConnect();
+});
