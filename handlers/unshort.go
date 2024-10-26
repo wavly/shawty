99e0580d88367a4120b935ff6bab6fe677fed336
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-10-26 17:30:13 +0000

    feat: caching the result of the request for unshorten the URL

--- handlers/unshort.go
+++ handlers/unshort.go
@@ -5,7 +5,9 @@ import (
 	"html/template"
 	"net/http"
 
+	"github.com/patrickmn/go-cache"
 	"github.com/wavly/shawty/asserts"
+	. "github.com/wavly/shawty/cache"
 	"github.com/wavly/shawty/internal/database"
 	"github.com/wavly/shawty/utils"
 	"github.com/wavly/shawty/validate"
@@ -26,23 +28,33 @@ func Unshort(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	db := utils.ConnectDB()
-	defer db.Close()
-	queries := database.New(db)
+	originalUrl, found := Cache.Get(code)
+	if !found {
+		Logger.Warn("Cache Miss: code for the URL isn't in the cache", "code", code, "user-agent", r.UserAgent())
+		db := utils.ConnectDB()
+		defer db.Close()
+		queries := database.New(db)
 
-	ret, err := queries.GetOriginalUrl(r.Context(), code)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			Logger.Warn("url doesn't exists in the database", "code", code, "user-agent", r.UserAgent(), "error", err)
-			asserts.NoErr(errorTempl.Execute(w, "There is no destination URL for this short URL: "+code), "Failed to execute template short-link-error.html")
+		ret, err := queries.GetOriginalUrl(r.Context(), code)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				Logger.Warn("url doesn't exists in the database", "code", code, "user-agent", r.UserAgent(), "error", err)
+				asserts.NoErr(errorTempl.Execute(w, "There is no destination URL for this short URL: "+code), "Failed to execute template short-link-error.html")
+				return
+			}
+
+			Logger.Error("failed to query to get the original url", "code", code, "user-agent", r.UserAgent(), "error", err)
+			asserts.NoErr(errorTempl.Execute(w, "Sorry, unexpected error occur when querying the database"), "Failed to execute short-link-error.html")
 			return
 		}
 
-		Logger.Error("failed to query to get the original url", "code", code, "user-agent", r.UserAgent(), "error", err)
-		asserts.NoErr(errorTempl.Execute(w, "Sorry, unexpected error occur when querying the database"), "Failed to execute short-link-error.html")
+		Cache.Set(code, ret, cache.DefaultExpiration)
+		templ := template.Must(template.ParseFiles("./partial-html/unshort-link.html"))
+		asserts.NoErr(templ.Execute(w, ret), "Failed to execute template unshort-link.html")
 		return
 	}
 
+	Logger.Info("Cache Hit: code for the URL found in the cache", "code", code, "url", originalUrl, "user-agent", r.UserAgent())
 	templ := template.Must(template.ParseFiles("./partial-html/unshort-link.html"))
-	asserts.NoErr(templ.Execute(w, ret), "Failed to execute template unshort-link.html")
+	asserts.NoErr(templ.Execute(w, originalUrl), "Failed to execute template unshort-link.html")
 }
