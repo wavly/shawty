1d91d030254b0ba63de109cce5abac459a5ea955
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-10-28 18:05:03 +0000

    refactor: live-reload server code

--- live-reload/src/main.rs
+++ live-reload/src/main.rs
@@ -1,37 +1,43 @@
 use notify::{Event, RecursiveMode, Watcher};
 
-use std::{net::TcpListener, path::Path, sync::mpsc, thread};
+use std::{
+  net::{TcpListener, TcpStream},
+  path::Path,
+  sync::mpsc,
+  thread,
+};
 
 fn main() {
   println!("Listening on: 9090");
   println!("Watching the filesystem");
-  let server = TcpListener::bind("0.0.0.0:9090").unwrap();
+  let listener = TcpListener::bind("0.0.0.0:9090").unwrap();
 
-  for stream in server.incoming() {
-    thread::spawn(|| {
-      let stream = stream.expect("tcp stream");
-      let mut websocket = tungstenite::accept(stream).expect("tcp stream accept");
-      let (tx, rx) = mpsc::channel::<Result<Event, notify::Error>>();
+  for stream in listener.incoming() {
+    thread::spawn(|| accept_conns(stream.expect("tcp stream")));
+  }
+}
 
-      let mut watcher = notify::recommended_watcher(tx).expect("watcher");
-      watcher
-        .watch(Path::new("../../"), RecursiveMode::Recursive)
-        .expect("watching files");
+fn accept_conns(stream: TcpStream) {
+  let mut websocket = tungstenite::accept(stream).expect("tcp stream accept");
+  let (tx, rx) = mpsc::channel::<Result<Event, notify::Error>>();
 
-      // Blocks forever
-      for res in rx {
-        match res {
-          Ok(event) => {
-            let kind = event.kind;
-            if kind.is_remove() || kind.is_modify() || kind.is_create() {
-              if websocket.send("file changed".into()).is_err() {
-                break;
-              }
-            }
+  let mut watcher = notify::recommended_watcher(tx).expect("watcher");
+  watcher
+    .watch(Path::new("../../"), RecursiveMode::Recursive)
+    .expect("watching files");
+
+  // Blocks forever
+  for res in rx {
+    match res {
+      Ok(event) => {
+        let kind = event.kind;
+        if kind.is_remove() || kind.is_modify() || kind.is_create() {
+          if websocket.send("file changed".into()).is_err() {
+            break;
           }
-          Err(err) => eprintln!("Erorr receiving events: {err}"),
         }
       }
-    });
+      Err(err) => eprintln!("Erorr receiving events: {err}"),
+    }
   }
 }
