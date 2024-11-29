49d31bb182ba4cbbf8a1740fae2df94fe2232e0f
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-11-29 11:26:13 +0000

    feat: made EvictOldLinks functions none blocking to make it easier to unit test

--- validate/evict_old_links.go
+++ validate/evict_old_links.go
@@ -2,50 +2,38 @@ package validate
 
 import (
 	"context"
+	"database/sql"
 	"time"
 
-	"github.com/wavly/shawty/asserts"
 	"github.com/wavly/shawty/internal/database"
 	prettylogger "github.com/wavly/shawty/pretty-logger"
-	"github.com/wavly/shawty/utils"
 )
 
 // Evict Old links form the database
-//
-// The function runs periodically and blocks the thread forever, use a
-// goroutine to run the function in a separate goroutine.
-func EvictOldLinks(mins time.Duration) {
-	asserts.AssertEq(mins <= 0, "The time to evict the old links from the database has to be greater than 0")
+func EvictOldLinks(db *sql.DB) {
 	logger := prettylogger.GetLogger(nil)
-	sleepDuration := time.Minute * mins
-	queries := database.New(utils.ConnectDB())
+	queries := database.New(db)
+	ctx := context.Background()
 
-	for {
-		rows, err := queries.GetLastAccessedTime(context.Background())
+	rows, err := queries.GetLastAccessedTime(ctx)
 
-		// Don't assert if failed to query last_accessed from the database because
-		// it will then crash the whole server!
-		if err != nil {
-			logger.Error("Failed to query last_accessed", "error", err)
-			time.Sleep(sleepDuration)
-			continue
-		}
+	// Don't assert if failed to query last_accessed from the database because
+	// it will then crash the whole server!
+	if err != nil {
+		logger.Error("Failed to query last_accessed", "error", err)
+		return
+	}
 
-		for _, row := range rows {
-			now := time.Now()
-			oneMonthAgo := now.AddDate(0, -1, 0)
+	for _, row := range rows {
+		now := time.Now()
+		oneMonthAgo := now.AddDate(0, -1, 0)
 
-			if row.LastAccessed.Before(oneMonthAgo) {
-				logger.Info("Link is older than a month", "link", row.OriginalUrl)
-				err = queries.DeleteLinkLastAccessed(context.Background(), row.LastAccessed)
-
-				if err != nil {
-					logger.Error("Failed to delete the link from the database", "error", err)
-					continue
-				}
+		if row.LastAccessed.Before(oneMonthAgo) {
+			logger.Info("Link is older than a month", "link", row.OriginalUrl)
+			if err = queries.DeleteLinkLastAccessed(ctx, row.LastAccessed); err != nil {
+				logger.Error("Failed to delete the link from the database", "error", err)
+				return
 			}
 		}
-
-		time.Sleep(sleepDuration)
 	}
 }
