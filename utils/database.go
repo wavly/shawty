b27baf33dc111c783662bbc0bd7eb5a5281932cc
Hamza <72913359+Hamza12700@users.noreply.github.com>
2025-02-09 16:12:50 +0000

    feat: remove env package and using only config package for configuration
    The `env` package was just a hacky way for getting around the cyclic
    import because the `config` package was using the `asserts` package
    which imports the `pretty-logger` package and that package imports the
    `config` package to determined what logger to return at runtime creating
    a cyclic dependency.
    
    Now instead using the `asserts` package in the `config` package it's
    simlpy checking for errors and call `log.Fatalln` if something gose
    wrong.

--- utils/database.go
+++ utils/database.go
@@ -5,14 +5,14 @@ import (
 	"fmt"
 
 	"github.com/wavly/surf/asserts"
-	"github.com/wavly/surf/env"
+	"github.com/wavly/surf/config"
 
 	_ "github.com/tursodatabase/go-libsql"
 )
 
 func ConnectDB() *sql.DB {
-	if env.MODE == "prod" {
-		db, err := sql.Open("libsql", fmt.Sprintf("%s?authToken=%s", env.TURSO_URL, env.TURSO_TOKEN))
+	if config.MODE == "prod" {
+		db, err := sql.Open("libsql", fmt.Sprintf("%s?authToken=%s", config.TURSO_URL, config.TURSO_TOKEN))
 		asserts.NoErr(err, "Failed to connect to Turso remote db")
 		return db
 	}
